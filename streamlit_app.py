b8fd517131af86604295b244f642356a1c31df3e
Sanvi <rathoresanvi3@gmail.com>
2025-06-09 16:05:05 +0530

    added Google Analytics

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,19 @@ import industry_trends
 import mock_interview
 import resume_matcher
 import skill_builder
+import streamlit.components.v1 as components
+
+ga_code = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-W2CRK5T61J"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-W2CRK5T61J');
+</script>
+"""
 
 def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
